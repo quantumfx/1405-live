d534cd8f4fa72b8a9f6e8aa6a23635dd72de03fc
Fang Xi Lin <github@fxl.gg>
2022-06-12 23:29:06 -0400

    improved camera script

--- scripts/take_photo.sh
+++ scripts/take_photo.sh
@@ -6,5 +6,5 @@ FILENAME=${DATETIME}.jpg
 FILE=${DIR}/${FILENAME}
 
 killall libcamera-still
-libcamera-still --vflip --hflip -o ${FILE}
+libcamera-still --vflip --hflip --immediate -t 60000 -o ${FILE}
 scp ${FILE} sagan:~/rpi_live/photos/${FILENAME}
