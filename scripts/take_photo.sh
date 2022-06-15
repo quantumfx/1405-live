c43f10bb22786332883c245fffa553e4e81b477a
Fang Xi Lin <github@fxl.gg>
2022-06-15 06:23:24 +0000

    long exposure

--- scripts/take_photo.sh
+++ scripts/take_photo.sh
@@ -6,5 +6,5 @@ FILENAME=current.jpg
 FILE=$DIR/$FILENAME
 
 killall libcamera-still
-libcamera-still --vflip --hflip --immediate -t 30000 -o $FILE --shutter 9999
+libcamera-still --vflip --hflip --immediate -t 30000 -o $FILE --shutter 9000
 rsync -P $FILE sagan:~/rpi_live/photos/$FILENAME
