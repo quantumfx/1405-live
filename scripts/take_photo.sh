c756896a44ddbeff2a354f4ad31e618b74ad929b
Fang Xi Lin <github@fxl.gg>
2022-06-15 02:41:11 -0400

    normal exposure

--- scripts/take_photo.sh
+++ scripts/take_photo.sh
@@ -6,5 +6,5 @@ FILENAME=current.jpg
 FILE=$DIR/$FILENAME
 
 killall libcamera-still
-libcamera-still --vflip --hflip --immediate -t 30000 -o $FILE --exposure long
+libcamera-still --vflip --hflip --immediate -t 30000 -o $FILE
 rsync -P $FILE sagan:~/rpi_live/photos/$FILENAME
