49c9044ab5f317cbc4662f77d05ff67b1742e368
Fang Xi Lin <github@fxl.gg>
2022-06-22 15:11:01 -0400

    reboot if camera module crashes

---
+++ scripts/auto_reboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATETIME=$(date +"%Y-%m-%dT%T")
+DIR=$HOME/rpi_live/photos
+FILENAME=current.jpg
+FILE=$DIR/$FILENAME
+UNIXTIME_CURRENT=$(date +"%s")
+UNIXTIME_FILE=$(date -r $FILE +"%s")
+TDIFF=$(expr $UNIXTIME_CURRENT - $UNIXTIME_FILE)
+
+if [ $TDIFF -gt 300 ];
+then
+    echo "It\'s been $TDIFF s since last photo was taken, rebooting at $DATETIME ."
+    sudo /sbin/shutdown -r now
+fi
