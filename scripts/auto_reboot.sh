9b2951896d39d2d5f55595d5adc5fecf6c720679
Fang Xi Lin <github@fxl.gg>
2022-06-23 11:12:12 -0400

    stricter reboot conditions

--- scripts/auto_reboot.sh
+++ scripts/auto_reboot.sh
@@ -4,12 +4,17 @@ DATETIME=$(date +"%Y-%m-%dT%T")
 DIR=$HOME/rpi_live/photos
 FILENAME=current.jpg
 FILE=$DIR/$FILENAME
+
 UNIXTIME_CURRENT=$(date +"%s")
 UNIXTIME_FILE=$(date -r $FILE +"%s")
 TDIFF=$(expr $UNIXTIME_CURRENT - $UNIXTIME_FILE)
 
-#if [ $TDIFF -gt 300 ];
-#then
-#    echo "It\'s been $TDIFF s since last photo was taken, rebooting at $DATETIME ."
-#    sudo /sbin/shutdown -r now
-#fi
+
+UPTIME=$(</proc/uptime)
+UPTIME=${UPTIME%%.*}
+
+if [[ $TDIFF -gt 300 ]] && [[ $UPTIME -gt 300 ]];
+then
+    echo "It\'s been $TDIFF s since last photo was taken, rebooting at $DATETIME ."
+    sudo /sbin/shutdown -r now
+fi
