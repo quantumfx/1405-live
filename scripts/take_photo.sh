3b0a0b82f84b92031579cf59b5dfc6e0a8b598c9
Fang Xi Lin <github@fxl.gg>
2022-06-28 13:39:17 -0400

    post-processing jsons

--- scripts/take_photo.sh
+++ scripts/take_photo.sh
@@ -6,5 +6,5 @@ FILENAME=current.jpg
 FILE=$DIR/$FILENAME
 
 killall libcamera-still
-libcamera-still --vflip --hflip --immediate -t 30000 -o $FILE
+libcamera-still --vflip --hflip --immediate -t 30000 --denoise cdn_hq -o $FILE --awb daylight #--post-process-file $HOME/rpi_live/scripts/post-processing/nightmode.json
 rsync -P $FILE sagan:~/rpi_live/photos/$FILENAME
